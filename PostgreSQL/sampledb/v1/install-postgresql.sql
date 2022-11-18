dad883472d12d59645af32f23416f35433ba6343
kamusiall <34232048+kamusiall@users.noreply.github.com>
2022-11-18 23:01:50 +0100

    Update install-postgresql.sql
    decreased generateticketactivity and generatetransferactivity

--- PostgreSQL/sampledb/v1/install-postgresql.sql
+++ PostgreSQL/sampledb/v1/install-postgresql.sql
@@ -33,7 +33,7 @@ AND    last.name_type  = 'LAST';
 select null as "Loading NFL and MLB teams";
 \i ./schema/functions/loadmlbteams.sql
 \i ./schema/functions/loadnflteams.sql
-select loadmlbteams();
+--select loadmlbteams();
 select loadnflteams();
 \i ./schema/functions/set_mlb_team_home_field.sql
 \i ./schema/functions/setnflhomefield.sql
@@ -44,21 +44,21 @@ select null as "Generating game seats";
 \i ./schema/functions/esubstr.sql
 \i ./schema/functions/generateseats.sql
 select generateseats();
-select generateseats();
-select generateseats();
-select generateseats();
+--select generateseats();
+--select generateseats();
+--select generateseats();
 
 -- loading mlb and nfl players
 select null as "Creating players";
 \i ./schema/functions/loadmlbplayers.sql
 \i ./schema/functions/loadnflplayers.sql
-select loadmlbplayers();
+--select loadmlbplayers();
 select loadnflplayers();
 
 -- generating mlb and nfl seasons
 select null as "Creating the MLB and NFL seasons";
 \i ./schema/functions/generatemlbseason.sql
-select generatemlbseason();
+--select generatemlbseason();
 \i ./schema/functions/generatenflseason.sql
 select generatenflseason();
 
@@ -67,18 +67,18 @@ select null as "Generating game tickets for MLB and NFL";
 \i ./schema/functions/generatesporttickets.sql
 -- generating football and baseball tickets
 select generatesporttickets('football');
-select generatesporttickets('baseball');
+--select generatesporttickets('baseball');
 
 -- Sell tickets and generating ticket activities
 select null as "Creating functions to sell and transfer tickets";
 \i ./schema/functions/generateticketactivity.sql
 -- generating some initial ticket purchases
-select generateticketactivity(5000);
+select generateticketactivity(50);
 
 -- Generating transfer activity procedures and views
 \i ./schema/functions/transferticket.sql
 \i ./schema/functions/generatetransferactivity.sql
-select generatetransferactivity(1000);
+select generatetransferactivity(10);
 
 -- adding Foreign Keys
 \i ./schema/foreign-keys.sql
